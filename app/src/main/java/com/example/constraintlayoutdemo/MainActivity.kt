60b9bee9e9c7e28e7adeb858ed3eed0898164b5b
NEELAM AGRAWAL <nilamagrawal605@gmail.com>
2020-08-09 01:01:58 +0530

    rounded bitmap

--- app/src/main/java/com/example/constraintlayoutdemo/MainActivity.kt
+++ app/src/main/java/com/example/constraintlayoutdemo/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.constraintlayoutdemo
 
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.devslopesprofilelogo)
+        val roundedLogo = RoundedBitmapDrawableFactory.create(resources, bitmap)
+        roundedLogo.cornerRadius = 15f
+
+        logo.setImageDrawable(roundedLogo)
     }
 }
